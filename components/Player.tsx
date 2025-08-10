1e60f71cffc1cde8d05036fd0c67de728f901f5d
Chaitany233patil <patilchaitany529@gmail.com>
2025-08-10 14:11:28 +0530

    fix the spotify error

--- components/Player.tsx
+++ components/Player.tsx
@@ -4,7 +4,7 @@ export default function Player() {
       data-testid="embed-iframe"
       className="border-radius:12px"
       src="https://open.spotify.com/embed/track/0FB5ILDICqwK6xj7W1RP9u?utm_source=generator"
-      width="50%"
+      width="100%"
       height="200"
       frameBorder="0"
       allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture"
