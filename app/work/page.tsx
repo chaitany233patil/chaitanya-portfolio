5584b936acbecb0562deedaf910869ee7b1afc1a
Chaitanya Patil <127408540+chaitany233patil@users.noreply.github.com>
2025-10-14 17:55:39 +0530

    Update Google Drive link in page.tsx

--- app/work/page.tsx
+++ app/work/page.tsx
@@ -11,7 +11,7 @@ export default function Work() {
             <div className="text-2xl font-bold">My Work</div>
             <a
               href={
-                "https://drive.google.com/file/d/16KkSWX_386nSSWOUGbSKMjaY9lhus8Jj/view?usp=sharing"
+                "https://drive.google.com/file/d/1QnyP4hIUxAtEFkY6MBR0TxZqdTMEOO0U/view?usp=sharing"
               }
               target="_blank"
             >
